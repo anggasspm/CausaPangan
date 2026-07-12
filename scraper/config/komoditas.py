cb9d87f1d496a1530f88bd61df9843a1e3e38ca8
Raeaw <raffi.darrell.f@gmail.com>
2026-07-12 23:13:53 +0700

    config: komoditas, wilayah, sumber

---
+++ scraper/config/komoditas.py
@@ -0,0 +1,7 @@
+KOMODITAS_KEYWORDS = {
+    "beras": ["beras", "gabah", "padi"],
+    "cabai_rawit_merah": ["cabai rawit", "cabe rawit"],
+    "cabai_merah_keriting": ["cabai merah", "cabe merah keriting"],
+    "bawang_merah": ["bawang merah"],
+    "minyak_goreng": ["minyak goreng", "migor"],
+}
