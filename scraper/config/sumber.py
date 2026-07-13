612dd0aad72799fa0328d94cc77095ad2d67ebd8
Raeaw <raffi.darrell.f@gmail.com>
2026-07-13 15:44:19 +0700

    config: update source

--- scraper/config/sumber.py
+++ scraper/config/sumber.py
@@ -2,6 +2,7 @@ SUMBER_BERITA = [
     {"nama": "Antara - Ekonomi", "rss": "https://www.antaranews.com/rss/ekonomi.xml"},
     {"nama": "Antara - Bisnis", "rss": "https://www.antaranews.com/rss/ekonomi-bisnis.xml"},
     {"nama": "Liputan6 - News", "rss": "https://feed.liputan6.com/rss/news"},
-    {"nama": "Kompas - Bisnis Keuangan", "rss": "https://www.kompas.com/getrss/bisniskeuangan"},
     {"nama": "Antara Jabar - Ekonomi", "rss": "https://jabar.antaranews.com/rss/ekonomi.xml"},
+    {"nama": "Antara Jatim - Ekonomi", "rss": "https://jatim.antaranews.com/rss/ekonomi.xml"},
+    {"nama": "Antara Jateng - Ekonomi", "rss": "https://jateng.antaranews.com/rss/ekonomi.xml"},
 ]
