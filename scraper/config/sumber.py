628c8c25492e8c9ed3489c7153acedccd0559b6e
Raeaw <raffi.darrell.f@gmail.com>
2026-07-13 15:36:21 +0700

    config: add source, update koomditas keywords

--- scraper/config/sumber.py
+++ scraper/config/sumber.py
@@ -3,4 +3,5 @@ SUMBER_BERITA = [
     {"nama": "Antara - Bisnis", "rss": "https://www.antaranews.com/rss/ekonomi-bisnis.xml"},
     {"nama": "Liputan6 - News", "rss": "https://feed.liputan6.com/rss/news"},
     {"nama": "Kompas - Bisnis Keuangan", "rss": "https://www.kompas.com/getrss/bisniskeuangan"},
+    {"nama": "Antara Jabar - Ekonomi", "rss": "https://jabar.antaranews.com/rss/ekonomi.xml"},
 ]
