fc9b69a7ba35696f4dd99fb62112a74747acae0d
Raeaw <raffi.darrell.f@gmail.com>
2026-07-15 20:20:20 +0700

    config: update config

--- scraper/config/wilayah.py
+++ scraper/config/wilayah.py
@@ -1,4 +1,38 @@
 WILAYAH_TARGET = {
-    "32": {"nama": "Jawa Barat", "kota": ["Bandung", "Bekasi", "Bogor", "Cirebon"]},
-    # tambah provinsi lain setelah keputusan Minggu 1
+    "32": {
+        "nama": "Jawa Barat",
+        "kota": [
+            "Bandung", "Bekasi", "Bogor", "Cirebon", "Depok",
+            "Sukabumi", "Cimahi", "Tasikmalaya", "Banjar",
+            "Karawang", "Purwakarta", "Subang", "Sumedang",
+            "Garut", "Ciamis", "Kuningan", "Majalengka",
+            "Indramayu", "Cianjur", "Pangandaran",
+        ],
+    },
+    "33": {
+        "nama": "Jawa Tengah",
+        "kota": [
+            "Semarang", "Surakarta", "Solo", "Pekalongan", "Tegal",
+            "Magelang", "Salatiga", "Purwokerto", "Banyumas",
+            "Cilacap", "Kudus", "Jepara", "Pati", "Rembang",
+            "Blora", "Grobogan", "Demak", "Kendal", "Batang",
+            "Pemalang", "Brebes", "Wonosobo", "Purbalingga",
+            "Banjarnegara", "Kebumen", "Sukoharjo", "Boyolali",
+            "Klaten", "Sragen", "Karanganyar", "Wonogiri",
+            "Temanggung", "Rembang",
+        ],
+    },
+    "35": {
+        "nama": "Jawa Timur",
+        "kota": [
+            "Surabaya", "Malang", "Kediri", "Blitar", "Madiun",
+            "Mojokerto", "Pasuruan", "Probolinggo", "Batu",
+            "Sidoarjo", "Gresik", "Jombang", "Lamongan", "Tuban",
+            "Bojonegoro", "Ngawi", "Magetan", "Ponorogo",
+            "Pacitan", "Trenggalek", "Tulungagung", "Nganjuk",
+            "Bangkalan", "Sampang", "Pamekasan", "Sumenep",
+            "Situbondo", "Bondowoso", "Jember", "Lumajang",
+            "Banyuwangi", "Pasuruan",
+        ],
+    },
 }
